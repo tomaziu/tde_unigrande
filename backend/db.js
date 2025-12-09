052bfec836b825727bb38d0d546e24b2432f6a45
tomaziu <thomazdemoraisnunes2018@gmail.com>
2025-12-09 11:36:40 -0300

    troca do index e login

--- backend/db.js
+++ backend/db.js
@@ -3,19 +3,19 @@ const mysql = require("mysql2/promise");
 async function connect() {
   try {
     const connection = await mysql.createConnection({
-      host: "bh36dfbstunvlbw8qstc-mysql.services.clever-cloud.com",
-      user: "uzs6cu1wlstbnaes",
-      password: "6tXiHRJMLSv9CaAoMOG3",
-      database: "bh36dfbstunvlbw8qstc",
-      port: 3306
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port: process.env.DB_PORT || 3306
     });
 
-    console.log("🟢 MySQL conectado com sucesso!");
+    console.log("Conectado ao MySQL!");
     return connection;
 
-  } catch (err) {
-    console.error("🔴 ERRO ao conectar no MySQL:", err);
-    throw err;
+  } catch (error) {
+    console.error("Erro ao conectar no MySQL:", error);
+    throw error;
   }
 }
 
